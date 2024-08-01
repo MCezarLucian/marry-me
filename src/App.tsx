5816c5a53c0ad4eff88e791b121f3ac4700ee952
MCezarLucian <guzar107@gmail.com>
2024-08-01 09:30:17 +0300

    feature/component/usertype/demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.css";
-import FileUploadDropzone from "./components/imageUploader/FileUploadDropzone";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { users } from "./mockdata/data";
-import ProfilePicture from "./components/imageUploader/ProfilePicture";
+import UserTypeComponent from "./components/usertypecomponent/UserTypeComponent";
 
 function App() {
   return (
-    <div className="h-32 w-[60%]">
+    <div className="h-full w-[60%]">
       <Navbar logged user={users[0]} />
-      <ProfilePicture profilePicture={users[0].profilePicture} />
-      <FileUploadDropzone user={users[0]} />
+      <UserTypeComponent participant={false} />
       <Footer />
     </div>
   );
