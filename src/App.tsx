701c15131ec8eb729b3c90d82571175b2610dd27
Galan Irinae <galanirinad@gmail.com>
2024-08-02 09:41:21 +0300

    merge comm

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { users } from "./mockdata/data";
-import UserTypeComponent from "./components/usertypecomponent/UserTypeComponent";
-import Bio from "./components/bio/Bio";
-import ModalDelete from "./components/modal-delete/ModalDelete";
+import ModalEdit from "./components/modalEdit/ModalEdit";
+
 function App() {
   return (
     <div className="h-full w-[60%]">
       <Navbar logged user={users[0]} />
-      <Bio user={users[0]} />
-      <ModalDelete></ModalDelete>
+      <ModalEdit user={users[0]}></ModalEdit>
       <Footer />
     </div>
   );
