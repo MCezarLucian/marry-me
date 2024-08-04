6806991e93b93d8aae2787679a5d8c10198e9ee3
MCezarLucian <guzar107@gmail.com>
2024-08-04 22:23:14 +0300

    feature/component/request_list/demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import { users } from "./mockdata/data";
-import ForgotPassword from "./components/forgotPassword/ForgotPassword";
+import Navbar from "./components/navbar/Navbar";
+import RequestsList from "./components/requestsList/RequestsList";
 function App() {
-  const [users, setUsers] = useState(initialUsers);
-
-  const handleDelete = (id: string) => {
-    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
-  };
-
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-100 border-b-2 border-gray-200">
       <Navbar logged user={users[0]} />
       <div className="h-[calc(100vh-143px)] w-[60%] flex justify-center items-center">
-        <ForgotPassword />
+        <RequestsList users={users} />
       </div>
       <Footer />
     </div>
---
+++ src/components/requestsList/RequestModal.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { UserType } from "@/lib/types";
+import { X } from "lucide-react";
+
+interface RequestModalProps {
+  user: UserType;
+  isOpen: boolean;
+  onClose: () => void;
+  approveRequest: (e: React.MouseEvent) => void;
+  rejectRequest: (e: React.MouseEvent) => void;
+}
+
+const handleModalClick = (e: React.MouseEvent) => {
+  e.stopPropagation();
+};
+
+const RequestModal: React.FC<RequestModalProps> = ({
+  user,
+  isOpen,
+  onClose,
+  approveRequest,
+  rejectRequest,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-10 flex items-center justify-center bg-opacity-50"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+    >
+      <div
+        onClick={handleModalClick}
+        className="font-Inter rounded-xl w-2/4 h-auto py-14 px-8 bg-white flex flex-col gap-6"
+      >
+        <div className="w-full flex justify-end">
+          <X onClick={onClose} className="cursor-pointer" />
+        </div>
+        <p className="text-4xl font-bold">Request</p>
+        <p className="text-4xl">
+          {user.firstName} {user.lastName}
+        </p>
+        <p className="w-full h-44 bg-gray-100 border border-gray-300 p-4 rounded">
+          {user.description}
+        </p>
+        <div className="flex flex-row w-full justify-center items-center gap-12">
+          <Button
+            variant="outline"
+            className="border-red-600 text-red-600 border-4"
+            onClick={rejectRequest}
+          >
+            Reject
+          </Button>
+          <Button
+            variant="outline"
+            className="border-green-600 text-green-600 border-4"
+            onClick={approveRequest}
+          >
+            Approve
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RequestModal;
