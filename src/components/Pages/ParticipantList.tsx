40e134d138c040e48063f017ad85f49e1e81d1ad
Galan Irinae <galanirinad@gmail.com>
2024-08-07 10:04:11 +0300

    Update Participant List Page

--- src/components/Pages/ParticipantList.tsx
+++ src/components/Pages/ParticipantList.tsx
@@ -6,6 +6,7 @@ import Chat from "../chat/Chat";
 
 interface ParticipantListProps {
   users: UserType[];
+  admin?: UserType;
   sender: UserType;
   receiver: UserType;
 }
@@ -14,6 +15,7 @@ const ParticipantList: React.FC<ParticipantListProps> = ({
   users,
   sender,
   receiver,
+  admin,
 }) => {
   const [filteredUsers, setFilteredUsers] = useState<UserType[]>(users);
   const [openChat, setOpenChat] = useState<boolean>(false);
@@ -37,14 +39,24 @@ const ParticipantList: React.FC<ParticipantListProps> = ({
     <div className="w-full flex flex-col relative bg-gray-100 border-gray-200 ">
       <Filter users={users} onFilterChange={handleFilterChange} />
       <div className="grid grid-cols-3 gap-4 justify-center items-center absolute top-10 left-1/4">
-        {filteredUsers.map((user) => (
-          <Card
-            key={user.id}
-            user={user}
-            openChat={openChat}
-            onClick={() => handleCardClick(user)}
-          />
-        ))}
+        {admin &&
+          filteredUsers.map((user) => (
+            <Card
+              key={user.id}
+              user={user}
+              openChat={openChat}
+              onClick={() => handleCardClick(user)}
+            />
+          ))}
+        {!admin &&
+          filteredUsers./* filter((user) => user.role). */ map((user) => (
+            <Card
+              key={user.id}
+              user={user}
+              openChat={openChat}
+              onClick={() => handleCardClick(user)}
+            />
+          ))}
       </div>
       {openChat && selectedUser && (
         <Chat
